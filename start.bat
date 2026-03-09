c43896a69a1e703a6d834c74f65898366d9c65d1
OSO-CodeProject <oleg_6813@mail.ru>
2026-03-09 23:38:58 +0500

    Add lobby startup mode and centralized mode router

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-srcds.exe -game csgo -console -tickrate 128 +map de_mirage -port 27015 +sv_setsteamaccount YOUR_GSLT_TOKEN_HERE
+srcds.exe -game csgo -console -tickrate 128 +map de_mirage +exec mode_lobby.cfg -port 27015 +sv_setsteamaccount YOUR_GSLT_TOKEN_HERE

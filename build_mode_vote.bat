ded2eb88f8ea3a64f39ee3431c865cf455a7bf7d
OSO-CodeProject <oleg_6813@mail.ru>
2026-03-16 22:24:19 +0500

    Harden build_serverflow.bat compiler detection and add build log guidance

--- build_mode_vote.bat
+++ build_mode_vote.bat
@@ -1,37 +1,7 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 
-set "ROOT=%~dp0"
-set "SPCOMP=%ROOT%csgo\addons\sourcemod\scripting\spcomp.exe"
-set "INCLUDE=%ROOT%csgo\addons\sourcemod\scripting\include"
-set "SRC=%ROOT%csgo\addons\sourcemod\scripting\mode_vote.sp"
-set "OUT=%ROOT%csgo\addons\sourcemod\plugins\mode_vote.smx"
-
-if not exist "%SRC%" (
-  echo [mode_vote build] source not found: %SRC%
-  exit /b 1
-)
-
-if not exist "%SPCOMP%" (
-  echo [mode_vote build] spcomp.exe not found: %SPCOMP%
-  if exist "%OUT%" (
-    echo [mode_vote build] using existing compiled plugin: %OUT%
-    exit /b 0
-  )
-  echo [mode_vote build] install full SourceMod package (with scripting compiler) or provide mode_vote.smx.
-  exit /b 2
-)
-
-echo [mode_vote build] compiling mode_vote.sp ...
-"%SPCOMP%" "%SRC%" -i "%INCLUDE%" -o "%OUT%"
-if errorlevel 1 (
-  echo [mode_vote build] compile failed.
-  if exist "%OUT%" (
-    echo [mode_vote build] falling back to existing plugin binary: %OUT%
-    exit /b 0
-  )
-  exit /b 3
-)
-
-echo [mode_vote build] compiled successfully: %OUT%
-exit /b 0
+REM Legacy compatibility shim.
+REM Canonical build entrypoint is build_serverflow.bat.
+call "%~dp0build_serverflow.bat"
+exit /b %errorlevel%
